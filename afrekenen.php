f223cc6eae337b525c350f16db809eba4219e709
MarthijnDonker <s1198899@student.windesheim.nl>
2023-11-23 10:43:33 +0100

    overig

--- afrekenen.php
+++ afrekenen.php
@@ -13,10 +13,24 @@ include __DIR__ . "/header.php";
             border: 1px solid #FFFFFF;
             padding: 10px;
         }
+        .WinkelmandjeTerug {
+            text-align: right;
+            padding: 10px;
+        }
+        .fa-shopping-cart {
+            color: #FFFFFF;
+            background-color: transparent;
+        }
     </style>
 </html>
 <body>
 
 <div class="opmaak">
 <h1>Afrekenen</h1>
+</div>
+
+<div class="WinkelmandjeTerug">
+<form action="winkelmandje.php" method="post">
+    <button type="submit"  class="fas fa-shopping-cart" id="AfrekenenKnop"> Terug Naar Winkelmandje</button>
+</form>
 </div>
