f12132fb3f3a63f79f2a525d831f521b471cc6aa
MarthijnDonker <s1198899@student.windesheim.nl>
2023-11-30 14:05:19 +0100

    stan is een kneus 3.0

--- afrekenen.php
+++ afrekenen.php
@@ -46,6 +46,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             text-align: center;
             border-radius: 20px;
             margin-top: 10px;
+            margin-left: 10px;
+            width: 120%;
         }
 
         .productenTonen table {
@@ -63,20 +65,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             border-radius: 20px;
             text-align: center;
             margin-top: 10px;
+            width: 100%;
+            margin-left: 110px;
         }
         .betalen {
             border: 1px solid #FFFFFF;
             border-radius: 20px;
             text-align: center;
             margin-top: 10px;
+            width: 77.5%;
+            margin-left: auto;
+            margin-right: 5px;
         }
 
         .fa-shopping-cart {
-            color: #FFFFFF;
-            background-color: transparent;
-        }
-
-        .fa-dollar {
             color: #FFFFFF;
             background-color: transparent;
         }
@@ -99,6 +101,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             margin-left: 50px;
 
         }
+        .IdealKnop {
+            color: #FFFFFF;
+            background-color: transparent;
+        }
+
     </style>
 </head>
 <body>
@@ -116,6 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <tr>
                     <th>Product</th>
                     <th>Aantal</th>
+                    <th>Product Prijs</th>
                     <th>Totaalprijs</th>
                 </tr>
                 </thead>
@@ -136,6 +144,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     if ($productDetails) {
                         print("<td>" . $productDetails['StockItemName'] . "</td>");
                         print("<td>" . $aantal . "</td>");
+                        print("<td>". $afgerondePrijs . "</td>");
                         print("<td>". totaalPrijsPerProduct($aantal,$afgerondePrijs));
                     }
                     print("</tr>");
@@ -155,6 +164,88 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <div class="gegevens">
         <h1>NAW-Gegevens</h1>
+        <div id="boxNAW">
+            <form id="NAW-Gegevens" method="post">
+                <table class="Naw Table">
+                    <tbody>
+                    <tr>
+                        <th><label for="FirstName">Voornaam</label></th>
+                        <td><input type="text" name="FirstName" id="FirstName" required></td>
+                    </tr>
+                    <tr>
+                        <th><label for="LastName">Achternaam</label></th>
+                        <td><input type="text" name="LastName" id="LastName" required></td>
+                    </tr>
+                    <tr>
+
+                        <th><label for="PostalPostalCode">Postcode</label></th>
+                        <td><input type="text" name="PostalPostalCode" id="PostalPostalCode" required></td>
+                    </tr>
+                    <tr>
+                        <th><label for="PostalAdressLine2">Stad</label></th>
+                        <td><input type="text" name="PostalAdressLine2" id="PostalAdressLine2" required></td>
+                    </tr>
+                    <tr>
+                        <th><label for="DeliveryAdressLine21">Straatnaam</label></th>
+                        <td><input type="text" name="yu_DeliveryAdressLine2" id="DeliveryAdressLine21" required></td>
+                    </tr>
+                    <tr>
+
+                        <th><label for="DeliveryAdressLine22">Huisnummer</label></th>
+                        <td><input type="text" name="xu_DeliveryAdressLine2" id="DeliveryAdressLine22" required></td>
+                    </tr>
+                    <tr>
+
+                        <th><label for="PhoneNumber">Telefoonnummer</label></th>
+                        <td><input type="text" name="PhoneNumber" id="PhoneNumber" required></td>
+                    </tr>
+                    <tr>
+                        <th><label for="Country">Land</label></th>
+                        <td>
+                            <select id="Country" id="Country" required>
+                                <?php
+                                $sql = "SELECT CountryName FROM Countries";
+                                $result = $databaseConnection->query($sql);
+
+                                // Generate options dynamically
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "<option value='" . $row['CountryName'] . "'>" . $row['CountryName'] . "</option>";
+                                }
+
+                                // Close connection
+                                $databaseConnection->close()
+                                ?>
+
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><button type="submit" value="Submit">Check</button></td>
+                    </tr>
+                    </tbody>
+                </table>
+
+
+
+            </form>
+        </div>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Retrieve values from the form
+            $DeliveryStreet = $_POST["yu_DeliveryAdressLine2"];
+            $DeliveryNumber = $_POST["xu_DeliveryAdressLine2"];
+
+            $FirstName = $_POST["FirstName"];
+            $Lastname = $_POST["LastName"];
+
+            // Combine the values into a single string
+            $DeliveryAdressLine = $DeliveryStreet . ' ' . $DeliveryNumber;
+            $CustomerName = $FirstName. " ". $Lastname;}
+
+
+      ?>
 
     </div>
 
@@ -162,32 +253,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h1>Betalen</h1>
         <div class="totaalPrijs">
             <h5>Totaalprijs: €<?php print($totaalPrijs) ?></h5>
-            <h8>Inclusief Btw</h8>
+            <h8>Inclusief Btw
 
+                <div class="naarIDealpagina">
+                    <form method="post" action="iDealdemopagina.php">
+                        <button type="submit" name="afrekenen" class="IdealKnop" id="AfrekenenKnop">Ideal</button>
+                    </form>
         </div>
-
-
-        <div class="naarIDealpagina">
-            <form method="post">
-                <button type='submit' name='betalen' class="fas fa-dollar" id="AfrekenenKnop">Betalen</button>
-            </form>
-        </div>
-    </div>
-</div>
-
 </body>
 </html>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['betalen'])) {
-
-foreach ($cart as $Artikelnummer => $aantal) {
-updateQuantityOnHand($Artikelnummer, $aantal, $connection);
-}
-
-mysqli_close($connection);
-
-unset($_SESSION['cart']);
-}
-?>
 
--- winkelmandje.php
+++ winkelmandje.php
@@ -79,6 +79,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             text-align: right;
             margin-right: 300px;
         }
+        .image {
+            margin-top: 15px;
+            margin-left: 10px;
+
+        }
 
     </style>
 </head>
@@ -91,6 +96,7 @@ $cart = getCart();
 $connection = connectToDatabase();
 $totaalPrijs = 0;
 $aantalProducten = 0;
+$afgerondePrijs = 0;
 
 //Toont de array $cart met artikelnummer als index//
 foreach ($cart as $Artikelnummer => $aantal) {
@@ -103,7 +109,7 @@ foreach ($cart as $Artikelnummer => $aantal) {
     if (isset($StockItemImage)) {
         if (count($StockItemImage) == 1) {
         } ?>
-            <div id="ImageFrameCart"
+            <div id="ImageFrameCart" class="image"
                  style="background-image: url('Public/StockItemIMG/<?php print $StockItemImage[0]['ImagePath']; ?>'); background-size: 155px;  border: 1px solid #FFFFFF background-repeat: no-repeat; background-position: center;">
             </div>
             
@@ -125,7 +131,8 @@ foreach ($cart as $Artikelnummer => $aantal) {
                 print("<div class='aantal'>");
                 print("<form method='post' style='display: inline;'>");
                 print("Aantal: ");
-                print($aantal);
+                print("<input type='text' name='aantal[$Artikelnummer]' value='$aantal'>");
+                print("<button hidden type='submit' name='update_aantal' value='$Artikelnummer' class='transparent-button'>Update</button>");
                 print("<div class='aanpassen'>");
                 print("<button type='submit' name='toevoegen' value='$Artikelnummer' class='transparent-button fas fa-plus'></button>");
                 print("<button type='submit' name='verminderen' value='$Artikelnummer' class='transparent-button fas fa-minus'></button>");
@@ -138,6 +145,15 @@ foreach ($cart as $Artikelnummer => $aantal) {
             }
             $aantalProducten += ($aantal);
             $totaalPrijs += ($aantal * $afgerondePrijs);
+        }
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_aantal'])) {
+    $updateItemID = $_POST["update_aantal"];
+    $newAantal = max(1, $_POST['aantal'][$updateItemID]);
+
+    if (array_key_exists($updateItemID, $cart)) {
+        $cart[$updateItemID] = (int)$newAantal;
+        updateCart($cart);
         }
 }
 //Als er post plaatsvindt bij de prullenbak knop wordt het product uit de array verwijdert.
